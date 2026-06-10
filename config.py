e6925c0ae1269b6e11fef849e48e3173eaa21d79
siva09072004 <154991419+siva09072004@users.noreply.github.com>
2026-06-10 22:52:41 +0530

    Updated app.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-MONGO_URI = "mongodb+srv://sskshiva89:Sivakumar09072004@cluster0.bfi9eum.mongodb.net/?retryWrites=true&w=majority"
-DATABASE_NAME = "attendance_system"
+import os
+
+MONGO_URI = os.getenv("mongodb+srv://sskshiva89:Sivakumar09072004@cluster0.bfi9eum.mongodb.net/?retryWrites=true&w=majority")
